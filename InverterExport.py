c969ed6b7c00139baf11e5484f29edd274295538
CoD <caravanofdeath@icloud.com>
2020-05-22 14:22:45 +0100

    dsad

--- InverterExport.py
+++ InverterExport.py
@@ -137,10 +137,11 @@ class InverterExport(object):
                     continue
 
                 #dump raw data to log
+                import codecs
                 self.logger.debug('RAW received Packet (len={0}): '.format(
                     len(response))+':'.join(hex(ord(chr(x)))[2:].zfill(2) for x in bytearray(response))+'  '+re.sub('[^\x20-\x7f]', '', ''.join(chr(x) for x in bytearray(response))))
                 for a_ in range(len(response)):
-                    print(int.from_bytes(bytearray(response[a_:a_+1])))
+                    print(int(codecs.encode(bytearray(response[a_:a_+1]), 'hex'), 16))
 
                 if len(response) > 20:
                     msg = InverterMsg.InverterMsg(response, self.logger)
